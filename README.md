53917e37d1ac4dd048ee6bcb12dc8dea2fe040aa
izcoser <76838137+izcoser@users.noreply.github.com>
2022-08-16 23:44:18 -0300

    Upload files
    Right now this only creates a thread. I should add more test cases soon.

---
+++ README.md
@@ -0,0 +1,2 @@
+Testing suite for tch imageboard using Puppeteer.
+Creates and deletes posts and threads containing multiple different file types to check for errors.
---
+++ index.js
@@ -0,0 +1,22 @@
+console.log('Beginning automated tests with JavaScript disabled.');
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.setJavaScriptEnabled(false);
+    await page.goto('https://tchan.lol/t');
+    await page.evaluate(() => {
+        // I can use document.getElement... here to modify the DOM.
+        document.getElementById('text').innerText = 'puppeteer thread testing';
+        //document.getElementById('embedInputId').value = 'https://www.youtube.com/watch?v=jfKfPfyJRdk';
+        //document.getElementById('postButton').click();
+    });
+    // And I can also do it here.
+    const elementHandle = await page.$('input[id=fileInputId]');
+    await elementHandle.uploadFile('file.png');
+    await page.click('input[id=postButton]');
+    //await page.screenshot({path: 'example.png'});
+    await browser.close();
+})();
